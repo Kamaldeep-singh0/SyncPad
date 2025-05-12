a129e2f7d58926b977405d51541ac933b936e3fa
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-12 23:25:52 +0530

    Landing page

--- apps/web/app/page.tsx
+++ apps/web/app/page.tsx
@@ -5,17 +5,15 @@ function page() {
   return (
     <div>
       <NavBar/>
-      <div className='h-screen max-w-full grid grid-cols-2 bg-amber-200 p-20 '>
+      <div className='h-screen max-w-full grid grid-cols-2  p-20 '>
         <div className='flex flex-col justify-center justify-items-center'>
-          <h1 className='text-5xl font-bold'>Real-time Collaboration for Teams</h1>
-          <h2 className='text-xl text-zinc-600 pr-60'>SyncPad brings your team together with shared documents and whiteboards. Edit markdown, draw together, and collaborate in real-time.</h2>
+          <h1 className='text-5xl font-bold mb-3'>Real-time Collaboration for Teams</h1>
+          <h2 className='text-xl text-zinc-600 pr-36 mb-3'>SyncPad brings your team together with shared documents and whiteboards. Edit markdown, draw together, and collaborate in real-time.</h2>
            <div className='grid grid-cols-2 gap-3 '>
-         <button className="bg-blue-500 text-white font-semibold py-2 px-4 rounded-md">
-        Login
+         <button className="bg-gradient-to-r from-blue-500 to-purple-500 text-white font-semibold py-2 px-4 rounded-md">
+        Get Started
        </button>
-        <button className=' hover:bg-purple-500 rounded-md py-2 px-4 hover:text-white font-semibold '>
-        Sign Up
-       </button>
+        
     
       </div>
         </div>
