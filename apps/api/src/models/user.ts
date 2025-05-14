f7e453bda722adca925a33d60c8602f467b3e569
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-15 01:20:49 +0530

    NEW

--- apps/api/src/models/user.ts
+++ apps/api/src/models/user.ts
@@ -1,6 +1,16 @@
 import mongoose from "mongoose";
+import bcrypt from 'bcrypt';
+const SALT_WORK_FACTOR = 10;
 
 const {Schema} = mongoose ;
+
+interface IUser extends Document {
+  firstName: string;
+  lastName?: string;
+  email: string;
+  password: string;
+  comparePassword(candidatePassword: string): Promise<boolean>;
+}
 
 const userSchema = new Schema({
     firstName : {
@@ -29,6 +39,30 @@ const userSchema = new Schema({
         required : true
     }
 })
+
+userSchema.pre('save',(next)=>{
+    const user  = this;
+
+    if(!user.isModified('password')) return next();
+
+    bcrypt.genSalt(SALT_WORK_FACTOR,(err,salt)=>{
+        if(err) return next(err);
+
+        bcrypt.hash(user.password,salt,(err,hash)=>{
+            if(err) return next(err);
+
+            user.password =hash;
+            next();
+        });
+    });
+});
+
+userSchema.methods.comparePassword = function(candidatePassword : String, cb) {
+  bcrypt.compare(candidatePassword, this.password, (err, isMatch) => {
+    if (err) return cb(err);
+    cb(null, isMatch);
+  });
+};
 
 const User = mongoose.model('User',userSchema);
 
