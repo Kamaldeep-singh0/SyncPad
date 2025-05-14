c13a938281043e731429406eddd2e19f22bda3e9
Kamaldeep Singh <145911073+Kamaldeep-singh0@users.noreply.github.com>
2025-05-14 21:31:34 +0530

    backend-initiated

--- apps/api/src/index.ts
+++ apps/api/src/index.ts
@@ -1,9 +1,14 @@
-import express, { Request, Response } from 'express';
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
 
 const app = express();
 const port = process.env.PORT || 3001
 
-app.get('/', (req: Request, res: Response) => {
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res ) => {
   res.send('Hello, TypeScript Express!');
 });
 
